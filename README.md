396fc042fee1ed11fceaa4fe8e188d7d71d2f3d0
Emeka <jimbahamas15@gmail.com>
2022-04-29 01:24:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a devops project that requires the deplyment of a kubernetes cluster from AWS through the use of terraform.
+The cluster has three worker nodes, and has a cloud native image registry "Harbor" installed within a namespace.
+
+Argocd is deployed for CD, and work is going on to complete the deployment, secure the deployment and integrate the CD with gitactions for CI.
+
+I will update this section with other informations as i progress with the project.
---
+++ eks.tf
@@ -0,0 +1,37 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "17.24.0"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.21"
+  subnets         = module.vpc.private_subnets
+
+  tags = {
+    Environment = "planA-Emeka"
+    GithubRepo  = "terraform-aws-eks"
+    GithubOrg   = "terraform-aws-modules"
+  }
+
+  vpc_id = module.vpc.vpc_id
+
+  workers_group_defaults = {
+    root_volume_type = "gp2"
+  }
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t3a.large"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+    }
+  ]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
